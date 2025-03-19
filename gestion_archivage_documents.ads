aeb0bc77020fa1a8a821315b845ff8af8178d1ee
DanyKgb <dogni.kragbe@etu.univ-poitiers.fr>
2025-03-19 23:40:58 +0100

    Mise à jour automatique : Mer 19 mar 2025 23:40:58 CET

--- gestion_archivage_documents.ads
+++ gestion_archivage_documents.ads
@@ -5,3 +5,5 @@ Package Gestion_Archivage_Documents is
    Package Fichier_Archivage_Document is New Ada.Sequential_IO (T_docArchive);
    Use Fichier_Archivage_Document;
 End Gestion_Archivage_Documents;
+
+oupsiii non
